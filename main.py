af2a35d38346c76444e950291710c64c551582b7
Mond <herecomesthemoon@protonmail.com>
2023-04-17 09:38:19 +0200

    Moved utilities to utilities.py

--- main.py
+++ main.py
@@ -1,65 +1,10 @@
-from SPARQLWrapper import SPARQLWrapper, JSON
-from typing import NamedTuple
-from pprint import pprint
+# from pprint import pprint
 from random import choice
 import abc
 from math import log10
 from itertools import chain
+from utilities import SPARQL, id_to_label, LENGTH_ID_PREFIX
 
-USER_AGENT: str = "JustTestingForNow/0.0 (testing@protonmail.ch)"
-LENGTH_ID_PREFIX: int = len("http://www.wikidata.org/entity/")
-
-SPARQL = SPARQLWrapper("https://query.wikidata.org/sparql")
-SPARQL.addCustomHttpHeader("User-Agent", USER_AGENT)
-SPARQL.setReturnFormat(JSON)
-
-
-class Country(NamedTuple):
-    name: str
-    id: str
-
-
-def query_countries():
-    sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
-    sparql.addCustomHttpHeader("User-Agent", USER_AGENT)
-    sparql.setReturnFormat(JSON)
-    query = """
-        SELECT DISTINCT ?entity ?entityLabel WHERE {
-            ?entity wdt:P31 wd:Q6256 . 
-            ?article schema:about ?entity .
-            ?article schema:isPartOf <https://en.wikipedia.org/>.
-            FILTER NOT EXISTS {?entity wdt:P31 wd:Q3024240}
-            FILTER NOT EXISTS {?entity wdt:P31 wd:Q28171280}
-            OPTIONAL { ?entity wdt:P576 ?dissolved } .
-            FILTER (!BOUND(?dissolved)) 
-            SERVICE wikibase:label { bd:serviceParam wikibase:language "en" . }
-        }
-        ORDER BY ?entityLabel
-    """
-    sparql.setQuery(query)
-    ret = sparql.queryAndConvert()
-    for val in ret["results"]["bindings"]:
-        yield Country(
-            val["entityLabel"]["value"],
-            val["entity"]["value"][LENGTH_ID_PREFIX:]
-        )
-
-
-def id_to_label(id: str) -> str:
-    query = f"""
-        SELECT  *
-        WHERE {{
-                wd:{id} rdfs:label ?label .
-                FILTER (langMatches( lang(?label), "EN" ) )
-              }} 
-        LIMIT 1
-    """
-    SPARQL.setQuery(query)
-    try:
-        return SPARQL.queryAndConvert()['results']['bindings'][0]['label']['value']
-    except:
-        print(f"Could not fetch label of wd:{id} from Wikidata.")
-        return id
 
 
 class Akinator:
@@ -257,24 +202,6 @@ class BoundNearWater(Bound):
         return self.format(val)
 
     
-def query_properties(props: list):
-    sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
-    sparql.addCustomHttpHeader("User-Agent", USER_AGENT)
-    sparql.setReturnFormat(JSON)
-    query = """
-    SELECT DISTINCT ?property (COUNT (?obj) AS ?occurrences)
-    WHERE { 
-        ?obj ?property ?value .
-    }
-    GROUP BY ?property
-    """
-    sparql.setQuery(query)
-    ret = sparql.queryAndConvert()
-    for val in ret["results"]["bindings"]:
-        yield Country(
-            val["entityLabel"]["value"],
-            val["entity"]["value"][LENGTH_ID_PREFIX:]
-        )
 
 
 if __name__ == '__main__':
